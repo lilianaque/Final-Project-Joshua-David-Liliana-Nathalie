28dc30044ccb497d5cedb2cf352bc2469e2cedf4
David Oyekeye <davidoyekeye@Davids-MacBook-Pro.local>
2023-08-29 20:07:36 -0500

    add t shirt service layer

--- gamestore/src/main/java/com/company/gamestore/service/ServiceLayer.java
+++ gamestore/src/main/java/com/company/gamestore/service/ServiceLayer.java
@@ -1,4 +1,17 @@
 package com.company.gamestore.service;
 
+import com.company.gamestore.repositories.TShirtsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Component
+@ComponentScan("com.company.gamestore.repositories")
 public class ServiceLayer {
+    private TShirtsRepository tShirtsRepository;
+
+    @Autowired
+    public ServiceLayer(TShirtsRepository tShirtsRepository) {
+        this.tShirtsRepository = tShirtsRepository;
+    }
 }
